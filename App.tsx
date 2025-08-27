6c767534026cb0b452ba22d7a7eb8ad787ae3c16
Dnnas05 <st10490363@vcconnect.edu.za>
2025-08-27 14:53:17 +0200

    Added animation

--- App.tsx
+++ App.tsx
@@ -1,114 +1,120 @@
 import { StatusBar } from 'expo-status-bar';
-import { SectionList, StyleSheet, Text, TextInput, View , Button,Image } from 'react-native';
-import { useState} from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { Text, SafeAreaView, StyleSheet,TextInput,Button,Image,View, Animated} from 'react-native';
+import {useState , useRef , useEffect} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const Stack= createNativeStackNavigator();
- 
+
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return(
     <NavigationContainer>
-       <Stack.Navigator>
-          <Stack.Screen name="Home" component={MainScreen}/>
-          <Stack.Screen name="ViewDetails" component={ViewDetails}/>
-       </Stack.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={MainScreen} />
+        <Stack.Screen name="ViewDetails" component={ViewDetails} />
+      </Stack.Navigator>
     </NavigationContainer>
 
 
-  )
+  );
+};
+
+function ViewDetails({ navigation, route}:any) {
+  const NameGet = route.params.NameSend;
+  const SurnameGet = route.params.SurnameSend;
+  return(
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text style={{fontSize:34,color: 'purple'}}>Name : {NameGet}, Surname : {SurnameGet}</Text>
+    </View>
+  );
+};
+
+
+
+//Animation Component
+const FadeInVeiw = (props:any) => {
+  const fadeAnim = useRef(new Animated.Value(0)).current
 }
 
 
-function MainScreen() {
-  
-  const[Name,setName] = useState('')
-  const[Surname,setSurname] = useState('')
+
+
+export function MainScreen({navigation}:any) 
+{
+  const [Name, setName] = useState('');
+  const [Surname, setSurname] = useState('');
   console.log("App starting up now.")
   return (
-    
+  
     <View>
-
-      <View style={styles.mainPicture}> 
-        <Image  style ={styles.ImageSize}
-        source = {require('./assets/React_native_img.png')}/>
-        
+      <View style={styles.mainPicture}>
+        <Image style={styles.ImageSize}
+        source={require('./assets/reactNativeImage.png')}/>
       </View>
+          
+        <Text style={styles.welcomeText}> Welcome your react app!</Text>
 
-       <Text style = {styles.welcomeText}>Welcome your React App!</Text> 
-       <View style={styles.InputFlex}>
-         <Text style ={styles.TextName}>Enter Name: </Text>
-         <TextInput style ={styles.TextName}
-         placeholder="Name"
-         onChangeText={newText => setName(newText)} />
-        </View>
-        <View style={styles.InputFlex}>
-       <Text style ={styles.TextSurname}>Enter Surname: </Text>
-       <TextInput style ={styles.TextName} 
-       placeholder= "Surname"
-       onChangeText={newText => setSurname(newText)}/>
-        </View>
-       <Button title = "Add user" onPress={() => {console.log("The user's name and surname is:" + Name + "Surname:" + Surname )}}/>
-       <StatusBar style="auto" />
-      
+      <View style={styles.InputFlex}>
+        <Text style={styles.HeadingText} >Enter Name:</Text>
+        <TextInput style={styles.InputBoxs}
+                   placeholder="First Name :"
+                   onChangeText={newText => setName(newText)}
+                   />
       </View>
-      
+      <View style={styles.InputFlex}>
+        <Text style={styles.HeadingText}>Enter Surname: </Text>
+        <TextInput style={styles.InputBoxs} 
+                    placeholder="Surname :"
+                    onChangeText={newText => setSurname(newText)}/>
+      </View>
+        <Button title = "Add User" 
+              onPress={()=>{
+                navigation.navigate('ViewDetails', 
+                  { NameSend : Name,
+                  SurnameSend : Surname});
+                  console.log("The users name is: " + Name + " Surname: " + Surname)}}/>
+    </View>
+    
+       
+    
   );
 }
-
-function ViewDetails({navigation,route}:any){
-  const NameGet = route.params.NameSend ; 
-  const SurnameGet = route.params.SunameSend;
-
-  return(
-    <View style = {{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Name : {NameGet} Surname: {SurnameGet}</Text>
-    </View>
-  )
-}
-
 
 const styles = StyleSheet.create({
   welcomeText: {
     paddingTop: 40,
     color: 'purple',
     fontWeight: 'bold',
-    fontSize: 40,
+    fontSize: 28,
     textAlign: 'center'
-    
-  },
-  
-  ImageSize: {
-    width:350,
-    height:350
-    
   },
 
-  mainPicture:{
+  ImageSize: {
+    width: 350,
+    height: 350
+  },
+
+  mainPicture: {
     paddingTop: 40,
     justifyContent: 'center',
-    alignItems: 'center'
-    
+    alignItems:'center'
   },
-  TextName:{
-    color:'red',
-    fontSize: 20,
-    
-  },
-  TextSurname:{
-    color: 'red',
-    fontSize: 20,
-    
 
-  },
   InputFlex:{
-    flexDirection: 'row',
+    flexDirection:'row',
     marginTop: 30,
-    justifyContent: 'space-evenly'
+    justifyContent: 'space-evenly',
+  },
 
+  InputBoxs:{
+    fontSize:32,
+    backgroundColor: 'yellow',
+    paddingHorizontal: 20,
+    width: 200,
+  },
+
+  HeadingText:{
+    fontSize:32,
   }
-  
-
-  
-  
 });
